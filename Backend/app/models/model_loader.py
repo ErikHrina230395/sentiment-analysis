4597e6e915df0b3758245ae6c1cbd3ff93f16b2f
0FilipSarik243537 <243537@buas.nl>
2025-02-06 18:53:04 +0100

    Fix backend pipeline path

--- Backend/app/models/model_loader.py
+++ Backend/app/models/model_loader.py
@@ -2,7 +2,7 @@ import dill
 
 def load_pipeline():
     """Loads the serialized Pipeline"""
-    model_path = 'model_files/pipeline.pkl'
+    model_path = '../../../ModelDevelopment/Final_model/pipeline.pk'
     with open(model_path, 'rb') as file:
         pipeline = dill.load(file)
     return pipeline
